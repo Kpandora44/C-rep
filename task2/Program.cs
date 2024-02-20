cf17fd2bb8f6990873cbc55b5a367debd1988f7b
Tatiana Koroleva <pandiracat@gmail.com>
2024-02-20 17:14:31 +0300

    Решение первой задачи для нахождения большего/меньшего числа

---
+++ task2/Program.cs
@@ -0,0 +1,21 @@
+﻿// который на вход принимает два числа и выводит, какое число большее, а какое меньшее.
+
+Console.WriteLine("FirstNUM: ");
+int FirstNUM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("SecondNUM: ");
+int SecondNUM = Convert.ToInt32(Console.ReadLine());
+
+if  (FirstNUM > SecondNUM)
+{
+    Console.WriteLine("первое число больше");
+}
+else if (FirstNUM < SecondNUM)
+{
+    Console.WriteLine("второе число больше");
+}
+else 
+{
+    Console.WriteLine("числа равны");
+}
+
+
