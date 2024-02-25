a571b31908b5dbd2ee83f437a5b4817b37fbee0c
Tatiana Koroleva <pandiracat@gmail.com>
2024-02-25 23:49:40 +0300

    Решение задачи 3

---
+++ seminar2-3/Program.cs
@@ -0,0 +1,12 @@
+﻿//Задача 3: Напишите программу, которая принимает на вход целое число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+Console.WriteLine("Введите число: ");
+
+int x = Convert.ToInt32(Console.ReadLine());
+
+
+int firstDigit = x / 10;
+int secondDigit = x % 10;
+int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
+
+Console.WriteLine(maxDigit);
