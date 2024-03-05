d18e85cdea2f77665ec6efc65ff04193753aa8ac
Tatiana Koroleva <pandiracat@gmail.com>
2024-03-05 19:51:57 +0300

    Как писать массивы

---
+++ Desktop/array/array1/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] array = new int[5];
+array[0] = 5;
+array[1] = 7;
+array[2] = 5;
+array[3] = 4;
+array[4] = 3;
+Console.WriteLine(array[0]);
+Console.WriteLine(array[1]);
+Console.WriteLine(array[2]);
+Console.WriteLine(array[3]);
+Console.WriteLine(array[4]);
+
+
+int[] array2 = new int[5] { 1, 2, 3, 4, 5 };
+
+// закоммитировать можно так- выделить код,нажать на правую кнопку и выбрать format document, код становится более читаемым
+
+int[] array3 = { 1, 5, 6, 8, 9 };    // упрощённая запись массива
+
+// на языке Python массив записывают так:
+// array = [1,2,3,4,5];
