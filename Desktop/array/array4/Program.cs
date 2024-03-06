0596be25fbd84e2149342d16f7984d3aa1725c5e
Tatiana Koroleva <pandiracat@gmail.com>
2024-03-06 21:02:17 +0300

    Решение с помощью нескольких способов

--- Desktop/array/array4/Program.cs
+++ Desktop/array/array4/Program.cs
@@ -19,6 +19,7 @@ while (i < n)
 }
 Console.WriteLine(max);
 
+// второй способ записи кода для задачи
 max = array[0];
 for(int j =0; j<n; j = j+1)
 {
@@ -30,7 +31,7 @@ for(int j =0; j<n; j = j+1)
   }
 Console.WriteLine(max);
 
-
+// третий способ записи кода для задачи
 
 max = array[0];
 foreach(int e in array)
