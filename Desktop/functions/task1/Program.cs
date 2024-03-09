1e2a2d0cf5d203d42b982260522ac292ed0e06f4
Tatiana Koroleva <pandiracat@gmail.com>
2024-03-09 14:34:43 +0300

    1Ф

---
+++ Desktop/functions/task1/Program.cs
@@ -0,0 +1,13 @@
+﻿double CalculataFormula(int a, int b, int c, int d)  // int - целочисленный тип данныйх
+{
+    double numenator = a * b;  // double - более вместительный тип данных, дробный
+    int denumenator = c + d;
+    double result = numenator / denumenator;
+    return result;
+}
+
+double result = CalculataFormula(1, 2, 3, 4);
+
+// double result- создаем переменную
+
+Console.Write(result);
